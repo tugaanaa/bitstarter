03a585d538572838df9738743aa13522561065d8
Battulga Purevragchaa <tugaanaa@gmail.com>
2013-07-29 23:10:29 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var content = fs.readFile('./index.html');
+  response.send(content.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
